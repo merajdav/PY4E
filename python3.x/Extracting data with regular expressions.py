fe76c9e9aab60a9b2b376aea37adc7efb68af0de
merajdav <67624081+merajdav@users.noreply.github.com>
2020-06-30 11:35:14 +0530

    Update Extracting data with regular expressions.py

--- python3.x/Extracting data with regular expressions.py
+++ python3.x/Extracting data with regular expressions.py
@@ -1,9 +1,12 @@
 import re
-count = 0
-hand = open("numbers.txt")
-lines = hand.read()
-match = re.findall('[0-9]+',lines)
-for i in match:
-	number = int(i)
-	count = count + number
-print (count)
+fh = open('regex_sum_613186.txt')
+numlist = list()
+for line in fh:
+    stuff = re.findall('[0-9]+', line)
+    if len(stuff) < 1:
+        continue
+    for anum in stuff:
+        num = int(anum)
+        numlist.append(num)
+print(numlist)
+print(sum(numlist))
